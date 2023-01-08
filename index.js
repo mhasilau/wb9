aca5d2e03ee8374952b23bcee8142578c7de567d
mhasilau <mikhail.hasilau@gmail.com>
2023-01-08 19:54:36 +0300

    js types

---
+++ index.js
@@ -0,0 +1,17 @@
+// const num = 5
+// console.log(typeof num);
+
+// console.log(typeof `${num}`);
+// console.log(typeof String(num));
+// console.log(typeof (num + ''));
+
+// const str = 'k10a14354'
+// console.log(typeof str);
+// const str1 = Number(str)
+// console.log(str1);
+
+// console.log(parseInt(str));
+
+const bool = 3564
+console.log(typeof Boolean(bool) );
+console.log(typeof !!bool );
