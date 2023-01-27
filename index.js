8739b3b705a7143b8d32273bbb200f1cb3240f7a
Mikhail Hasilau <78950448+mhasilau@users.noreply.github.com>
2023-01-27 19:45:05 +0300

    Update index.js
    upd

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-// const num = 5
-// console.log(typeof num);
+const num = -5
+console.log(typeof num, num);
 
-// console.log(typeof `${num}`);
-// console.log(typeof String(num));
-// console.log(typeof (num + ''));
+const numStr1 = String(num)
+console.log(typeof numStr1, numStr1);
 
-// const str = 'k10a14354'
-// console.log(typeof str);
-// const str1 = Number(str)
-// console.log(str1);
+const numBool1 = Boolean(num)
+console.log(typeof numBool1, numBool1);
 
-// console.log(parseInt(str));
+const numStr2 = 5 + ''
+console.log(typeof numStr2, numStr2);
 
-const bool = 3564
-console.log(typeof Boolean(bool) );
-console.log(typeof !!bool );
+const numBool2 = !!num
+console.log(typeof numBool2, numBool2);
+
+const str = '123'
+console.log(typeof str, str);
+
+const strNum1 = Number(str)
+console.log(typeof strNum1, strNum1);
+
+const strNum2 = parseInt(str)
+console.log(typeof strNum2, strNum2);
+
+const strNum3 = +str
+console.log(typeof strNum3, strNum3);
